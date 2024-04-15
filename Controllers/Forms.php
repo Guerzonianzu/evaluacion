f7599dea0184d6465885eef5559cdaedda826078
Guerzonianzu <cjbaez2012@gmail.com>
2024-04-15 17:58:58 -0300

    Update Forms.php

--- Controllers/Forms.php
+++ Controllers/Forms.php
@@ -14,9 +14,18 @@
 
             $sql = "select * from trabajadores tra join servicios ser on tra.servicio = ser.id_servicio where tra.jefe_inmediato = $this->id_jefe;";
 
-            $resultado = $con->query($sql);
+            try {
 
+                $resultado = $con->query($sql);
+
+            } catch (PDOException $e){
+
+                $con->bdError();
+                die();
+
+            }
             
+                       
 
         }
 
