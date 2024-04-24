5dd36ec70b218bc84f8c5fdbb7d9db73ddce4bfb
Guerzonianzu <cjbaez2012@gmail.com>
2024-04-23 21:31:22 -0300

    23-4-2024

--- Prev/infraestructura/RRHH016-A-ED-INFRAESTRUCTURA.php
+++ Prev/infraestructura/RRHH016-A-ED-INFRAESTRUCTURA.php
@@ -1,7 +1,7 @@
 <?php
     session_start();
     if($_SESSION['rol'] == 2){ 
-                                                                                                               
+
         include "../../Controllers/Conexion.php";
         include "../../Controllers/Forms.php";
         
